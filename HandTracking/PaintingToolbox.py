a88e1f5acd67747d9885769f8919471e4be22f07
srli19 <srli19@student.aau.dk>
2022-03-07 10:07:45 +0100

    changed linesize

--- HandTracking/PaintingToolbox.py
+++ HandTracking/PaintingToolbox.py
@@ -1,5 +1,5 @@
 class PaintingToolbox:
-    def __init__(self, size=10, color=None):
+    def __init__(self, size=5, color=None):
         if color is None:
             color = [150, 150, 150]
         self.line_size: int = int(size)
