fbd04847f825ceb0a3c2f5d970d6ad0fb6a3f26d
srli19 <srli19@student.aau.dk>
2022-03-08 11:59:40 +0100

    Make foundation for erasing, with thumb

--- HandTracking/PaintingToolbox.py
+++ HandTracking/PaintingToolbox.py
@@ -1,12 +1,11 @@
 class PaintingToolbox:
-    def __init__(self, size=5, color=None, mask_color=None):
+    def __init__(self, size=5, color=None):
         if color is None:
-            color = [150, 150, 150, 255]
-        if mask_color is None:
-            mask_color = [1, 1, 1, 1]
+            color = {}
+            color['WHITE'] = [150, 150, 150, 255]
+            color['BLACK'] = [1, 1, 1, 1]
 
         self.line_size: int = int(size)
         self.circle_size: int = int(size/2)
-        self.color: list = color
-        self.mask_color: list = mask_color
+        self.color: dict = color
         self.mask_circle: int = int(size*12)
