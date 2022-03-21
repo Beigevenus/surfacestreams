6731f044b0f901c2966878541371e5803f3e617a
mflp19 <54845694+mflp19@users.noreply.github.com>
2022-03-21 09:16:39 +0100

    add "active" button

--- HandTracking/Button.py
+++ HandTracking/Button.py
@@ -2,13 +2,12 @@ from HandTracking.Point import Point
 
 
 class Button:
-    def __init__(self, callback, color='WHITE'):
+    def __init__(self, callback, color: str = 'WHITE', active: bool = False):
         self.size: int = 25
         self.location: Point = Point(0, 0)
         self.callback = callback
-        self.selected: bool = False
+        self.active: bool = active
         self.color = color
 
     def set_location(self, point: Point):
         self.location = point
-
